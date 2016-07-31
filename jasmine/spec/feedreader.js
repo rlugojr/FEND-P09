d2cd37b625d7c7e7268e3c77001da5d7ed6f51d2
Ray Lugo, Jr <rlugojr@gmail.com>
2016-07-31 03:49:44 -0400

    removed test
    removed test that contains trim function because it is logically
    redundant.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -36,7 +36,6 @@ $(function() {
         it('has names for each feed.', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined;
-                expect(feed.name.trim()).not.toBe("");
                 expect(feed.name).not.toBeNull;
             }, this);
         });
